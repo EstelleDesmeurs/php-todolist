d49ae20eb5caed248d38a27cfa651a83f96803e5
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-06-12 12:08:29 +0200

    files

---
+++ form.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta> charset="UTF-8">
+	<title>To Do List</title>
+</head>
+<body>
+<form id="form" action="" method="post">
+    <textarea name="content" id="content"></textarea>
+    <input type="submit" id="submit" name="submit" value="Envoyer" />
+
+<?php
+
+
+
+?>
+
+</form>
+</body>
+</html>
