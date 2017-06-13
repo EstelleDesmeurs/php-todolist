99d492c2fef62156f1ced8dcb220de860878bd2d
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-06-13 16:19:24 +0200

    un php

--- form.php
+++ form.php
@@ -1,13 +1,3 @@
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>To Do List</title>
-</head>
-<body>
-<form id="form" action="" method="post">
-    <textarea name="content" id="content"></textarea>
-    <input type="submit" id="submit" name="submit" value="Envoyer"/>
-
 <?php
 
 $content = sanitize($_POST['content']);	
@@ -17,77 +7,55 @@ function sanitize($x) {
     return $x;
 }
 
-$content = sanitize($_POST['content']);
-
 function validate($y) {
     $y= filter_var($y, FILTER_VALIDATE_STRING);
     return $y;
 }
-  
 
-if (isset($_POST['content'])) {
-	if (!empty ($_POST['content'])) {
-		echo "La tâche a été ajoutée!";
-		$todojson = 'assets/js/todo.json';
-		//print_r($todojson);
-		$openFile = file_get_contents($todojson);
-		//print_r($openFile);
-		$obj = json_decode($openFile, true);
-		$save = array_push($obj, $content);
-		$test = json_encode($obj, JSON_PRETTY_PRINT);
-		$put = file_put_contents($todojson, $test);
-		//print_r($obj);
-	}
-	else {
-		echo "Ajoutez une tâche";
-	}
-}	
-
-
-
+function toDo(){     
+	if (isset($_POST['content'])) {
+			global $toDo;
+			global $done;
+			$toDo = [];
+			$done = [];
+		if (!empty ($_POST['content'])) {
+			$content = sanitize($_POST['content']);
+			//print_r($content);
+			$todojson = 'assets/js/todo.json';
+			//print_r($todojson);
+			$openFile = file_get_contents($todojson);
+			//print_r($openFile);
+			$obj = json_decode($openFile, true);
+			$save = array_push($obj, $content);
+			$test = json_encode($obj, JSON_PRETTY_PRINT);
+			$put = file_put_contents($todojson, $test);
+			//print_r($obj);
+		}
+		else {
+			echo "Ajoutez une tâche";
+		}
+	}	
+}
 
 /*
-function saveData($content){
-	$line = date('Y-m-d | H:i:s') . ',' . $content;
-	$lines = file_get_contents("todo.json");
-	$lines = $lines . PHP_EOL . $line;
-	file_put_contents ("/assets/js/todo.json", $lines);
+    $obj = array($id => array("task" => $value, "status" => "open"));
+    if(is_array($current)) {
+        $current = array_merge_recursive($obj, $current);
+    } else {
+        $current = $obj;
+    }
+    $current=json_encode($current); 
+    if(file_put_contents($file, $current, LOCK_EX)) {
+        echo"Task added";
+    } else {
+        echo"failure.";
+    }
 
-}
-
-saveData($content);
-
-$content = ($_POST['content']);
-echo json_encode($content);
-
-function printData(){
-	$lines = file_get_contents("data.csv");
-	$linesArray = explode(PHP_EOL, $lines);
-	rsort($linesArray);
-	foreach ($linesArray as $line){
-		echo "<tr>";
-		$dataArray = explode(',' , $line);
-		echo "<td>" . $dataArray[0] . "</td>";
-		echo "</tr>";
-	}
-}
-
-
-$lang = (file_exists('todo.json'));
-
-/* Récupération du contenu du fichier .json 
-$json = file_get_contents('todo'.$lang.'.json');
-/* Les données sont récupérées sous forme de tableau (true) 
-$tr = json_decode($json, true);
 */
-
-//tache va dans un tableau
-//tache stockée dans json
-// $_POST push dans le tableau
 
 
 ?>
 
-</form>
-</body>
-</html>
+
+
+
