7718c83897b0041aabf0333d65dd8b03cc576ef6
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-06-15 16:23:17 +0200

    changes

--- index.php
+++ index.php
@@ -9,37 +9,38 @@ require 'form.php';
 	<link href="https://fonts.googleapis.com/css?family=Catamaran|Questrial" rel="stylesheet">
 	<title>To Do List</title>
 </head>
+
 <body>
+	<main>
+		<div class="form" >
+			<div class="text">
+			<h1>My<br>Daily<br>To Do List</h1>
+			<form id="form" action="" method="post">
+			<textarea name="content" id="content"></textarea><br>
+			<input type="submit" id="todo" name="todo" value="Add Task"/>
 
-<main>
-	<div class="form" >
-		<h1>My<br>Daily<br>To Do List</h1>
+			<section class="afaire">
+				<h2>To Do</h2>
 
-<form id="form" action="" method="post">
-    <textarea name="content" id="content"></textarea><br>
+				<?php
+				toDo();
+				?>
 
-    <input type="submit" id="todo" name="todo" value="Envoyer"/>
-<section class="afaire">
-<h2>A faire</h2>
+			<input type="submit" id="done" name="done" value="Edit"/>
+			</section>
 
-<?php
-toDo();
-?>
+			<section class="archives">
+				<h2>Done</h2>
 
-<input type="submit" id="done" name="done" value="Enregistrer"/>
-</section>
+				<?php
+				displayDone();
+				?>
 
-<section class="archives">
-<h2>Archives</h2>
-
-<?php
-displayDone();
-?>
-
-</section>
-</form>
-	</div>
-
-</main>
+			</section>
+			</div>
+			</form>
+		</div>
+	</main>
+	<script type="text/javascript" href="assets/js/app.js"></script>
 </body>
 </html>
